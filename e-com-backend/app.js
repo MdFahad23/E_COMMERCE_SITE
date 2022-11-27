cd6509f1fee281a415a6399a4c590ea562817d70
Md Fahad <fahadbdctglh8@gmail.com>
2022-11-27 15:46:17 +0600

    User SignUP && Login and handel the middlewares error

--- e-com-backend/app.js
+++ e-com-backend/app.js
@@ -1,14 +1,21 @@
-require("dotenv/config");
+require("express-async-errors");
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
+const error = require("./middlewares/error");
+
+const userRouter = require("./routers/userRouter");
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
-if (process.env.NODE === "development") {
+if (process.env.NODE_ENV === "development") {
   app.use(morgan());
 }
+
+app.use("/api/user", userRouter);
+
+app.use(error);
 
 module.exports = app;
