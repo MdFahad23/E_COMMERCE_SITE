3cb709ec5f9def92d774e268e016a953b20ebc4b
Md Fahad <fahadbdctglh8@gmail.com>
2022-12-05 16:33:24 +0600

    Product Created and Update or Product Filtring Module

--- e-com-backend/app.js
+++ e-com-backend/app.js
@@ -1,23 +1,10 @@
 require("express-async-errors");
 const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
 const app = express();
 const error = require("./middlewares/error");
 
-const userRouter = require("./routers/userRouter");
-const categoryRouter = require("./routers/categoryRouter");
-
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(cors());
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan());
-}
-
-app.use("/api/user", userRouter);
-app.use("/api/category", categoryRouter);
-
+require("./middlewares")(app);
+require("./middlewares/routes")(app);
 app.use(error);
 
 module.exports = app;
