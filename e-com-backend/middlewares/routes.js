980eede7581c7804f4ee5007031f85670b9e6e7d
Md Fahad <fahadbdctglh8@gmail.com>
2022-12-06 16:57:40 +0600

    Create CartItem all CURD Operation

--- e-com-backend/middlewares/routes.js
+++ e-com-backend/middlewares/routes.js
@@ -1,9 +1,11 @@
 const userRouter = require("../routers/userRouter");
 const categoryRouter = require("../routers/categoryRouter");
 const productRouter = require("../routers/productRouter");
+const cartItemRouter = require("../routers/cartItemRouter");
 
 module.exports = (app) => {
   app.use("/api/user", userRouter);
   app.use("/api/category", categoryRouter);
   app.use("/api/product", productRouter);
+  app.use("/api/cart", cartItemRouter);
 };
